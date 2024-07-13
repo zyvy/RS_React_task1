3f564e7b07fded7262d66b6bcd51a9b7d5ffe058
zyvy <gm.zyvy@gmail.com>
2024-07-13 16:27:41 +0300

    feat: add hook

--- pokemon-app/src/components/SearchComponent.tsx
+++ pokemon-app/src/components/SearchComponent.tsx
@@ -5,7 +5,7 @@ interface SearchInputProps {
   onSearch: (searchTerm: string) => void;
 }
 
-function SearchComponent({ onSearch }: SearchInputProps) {
+const useSavedSearchTerm = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
 
   useEffect(() => {
@@ -14,6 +14,14 @@ function SearchComponent({ onSearch }: SearchInputProps) {
       setSearchTerm(savedSearchTerm);
     }
   }, []);
+
+  return { searchTerm, setSearchTerm };
+};
+
+
+function SearchComponent({ onSearch }: SearchInputProps) {
+  const {searchTerm, setSearchTerm} = useSavedSearchTerm();
+ // onSearch(searchTerm);
 
   const handleSearch = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -27,14 +35,10 @@ function SearchComponent({ onSearch }: SearchInputProps) {
 
   return (
     <form onSubmit={handleSearch}>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleChange}
-      />
+      <input type="text" value={searchTerm} onChange={handleChange} />
       <button type="submit">Search</button>
     </form>
   );
-};
+}
 
 export default SearchComponent;
--- pokemon-app/src/components/SearchResults.tsx
+++ pokemon-app/src/components/SearchResults.tsx
@@ -1,6 +1,5 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './SearchResults.css';
-
 interface SearchResultsProps {
   searchString: string;
 }
@@ -17,75 +16,51 @@ interface Anime {
   image_url: string;
 }
 
-interface SearchResultsState {
-  animes: Anime[];
-  loading: boolean;
-  error: string | null;
-}
 
-class SearchResults extends Component<SearchResultsProps, SearchResultsState> {
-  constructor(props: SearchResultsProps) {
-    super(props);
-    this.state = {
-      animes: [],
-      loading: false,
-      error: null,
+
+function SearchResults({ searchString }: SearchResultsProps) {
+  const [animes, setAnimes] = useState<Anime[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchAnime = async () => {
+      setLoading(true);
+      setError(null);
+      console.log(searchString);
+      const url = `https://api.jikan.moe/v4/anime?q=${searchString}`;
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setAnimes(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setError('Failed to fetch');
+        setLoading(false);
+      }
     };
-  }
-
-  componentDidMount() {
-    this.fetchAnimeData();
-  }
-
-  componentDidUpdate(prevProps: SearchResultsProps) {
-    if (prevProps.searchString !== this.props.searchString) {
-      this.fetchAnimeData();
-    }
-  }
-
-  async fetchAnimeData() {
-    const { searchString } = this.props;
-    const url = `https://api.jikan.moe/v4/anime?q=${searchString}`;
-
-    this.setState({ loading: true, error: null });
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      this.setState({
-        animes: data.data,
-        loading: false,
-      });
-    } catch (error) {
-      console.error(error);
-      this.setState({ error: 'Failed to fetch', loading: false });
-    }
-  }
-
-  render() {
-    const { searchString } = this.props;
-    const { animes, loading, error } = this.state;
-
-    return (
-      <div>
-        <h2>Search Results:</h2>
-        <div className="search_results">
-          <h2>{searchString}</h2>
-          {loading && <p>Loading...</p>}
-          {error && <p className="error">{error}</p>}
-          {animes.length > 0 && (
-            <div className="anime-list">
-              {animes.map((anime) => (
-                <div key={anime.mal_id} className="anime-card">
-                  <h3>{anime.title}</h3>
-                  <img src={anime.images.jpg.image_url} alt={anime.title} />
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+    fetchAnime();
+  }, [searchString]);
+  return (
+    <div>
+      <h2>Search Results:</h2>
+      <div className="search_results">
+        <h2>{searchString}</h2>
+        {loading && <p>Loading...</p>}
+        {error && <p className="error">{error}</p>}
+        {animes.length > 0 && (
+          <div className="anime-list">
+            {animes.map((anime) => (
+              <div key={anime.mal_id} className="anime-card">
+                <h3>{anime.title}</h3>
+                <img src={anime.images.jpg.image_url} alt={anime.title} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default SearchResults;
